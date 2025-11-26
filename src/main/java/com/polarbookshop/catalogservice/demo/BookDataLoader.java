4b22c79374396b879c80108a82d5072b0e8e56b3
Seongho Lee <tjdgh2626@gmail.com>
2025-11-26 17:35:44 +0900

    feat: 테스트

--- src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java
+++ src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java
@@ -11,7 +11,7 @@ import com.polarbookshop.catalogservice.domain.Book;
 import com.polarbookshop.catalogservice.domain.BookRepository;
 
 @Component
-@Profile("test-data")
+@Profile("testdata")
 public class BookDataLoader {
 	private final BookRepository bookRepository;
 
