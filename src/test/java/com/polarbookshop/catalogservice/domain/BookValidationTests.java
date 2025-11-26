f497580441c0ca4ac0a66614bbb6c21cea636e39
Seongho Lee <tjdgh2626@gmail.com>
2025-11-26 16:50:29 +0900

    feat: 5장 끝

--- src/test/java/com/polarbookshop/catalogservice/domain/BookValidationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/domain/BookValidationTests.java
@@ -1,5 +1,7 @@
 package com.polarbookshop.catalogservice.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -7,29 +9,27 @@ import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class BookValidationTests {
-    private static Validator validator;
+	private static Validator validator;
 
-    @BeforeAll
-    static void setUp() {
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        validator = factory.getValidator();
-    }
+	@BeforeAll
+	static void setUp() {
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		validator = factory.getValidator();
+	}
 
-    @Test
-    void whenAllFieldsCorrectThenValidationSucceeds() {
-        var book = new Book("1234567890", "Title", "Author", 9.90);
-        var violations = validator.validate(book);
-        assertThat(violations).isEmpty();
-    }
+	@Test
+	void whenAllFieldsCorrectThenValidationSucceeds() {
+		var book = Book.of("1234567890", "Title", "Author", 9.90, null);
+		var violations = validator.validate(book);
+		assertThat(violations).isEmpty();
+	}
 
-    @Test
-    void whenIsbnDefinedButIncorrectThenValidationFails() {
-        var book = new Book("a234567890", "Title", "Author", 9.90);
-        var violations = validator.validate(book);
-        assertThat(violations).hasSize(1);
-        assertThat(violations.iterator().next().getMessage()).isEqualTo("The ISBN format must be valid.");
-    }
+	@Test
+	void whenIsbnDefinedButIncorrectThenValidationFails() {
+		var book = Book.of("a234567890", "Title", "Author", 9.90, null);
+		var violations = validator.validate(book);
+		assertThat(violations).hasSize(1);
+		assertThat(violations.iterator().next().getMessage()).isEqualTo("The ISBN format must be valid.");
+	}
 }
--- src/test/java/com/polarbookshop/catalogservice/web/BookJsonTests.java
+++ src/test/java/com/polarbookshop/catalogservice/web/BookJsonTests.java
@@ -1,45 +1,46 @@
 package com.polarbookshop.catalogservice.web;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
-import static org.assertj.core.api.Assertions.assertThat;
 
 import com.polarbookshop.catalogservice.domain.Book;
 
 @JsonTest
 public class BookJsonTests {
 
-    @Autowired
-    private JacksonTester<Book> json;
+	@Autowired
+	private JacksonTester<Book> json;
 
-    @Test
-    void testSerialize() throws Exception {
-        // Arrange
-        var book = new Book("1234567890", "Title", "Author", 9.90);
+	@Test
+	void testSerialize() throws Exception {
+		// Arrange
+		var book = Book.of("1234567890", "Title", "Author", 9.90, null);
 
-        // Act
-        var jsonContent = json.write(book);
+		// Act
+		var jsonContent = json.write(book);
 
-        // Assert
-        assertThat(jsonContent).extractingJsonPathStringValue("@.isbn").isEqualTo(book.isbn());
-        assertThat(jsonContent).extractingJsonPathStringValue("@.title").isEqualTo(book.title());
-        assertThat(jsonContent).extractingJsonPathStringValue("@.author").isEqualTo(book.author());
-        assertThat(jsonContent).extractingJsonPathNumberValue("@.price").isEqualTo(book.price());
-    }
+		// Assert
+		assertThat(jsonContent).extractingJsonPathStringValue("@.isbn").isEqualTo(book.isbn());
+		assertThat(jsonContent).extractingJsonPathStringValue("@.title").isEqualTo(book.title());
+		assertThat(jsonContent).extractingJsonPathStringValue("@.author").isEqualTo(book.author());
+		assertThat(jsonContent).extractingJsonPathNumberValue("@.price").isEqualTo(book.price());
+	}
 
-    @Test
-    void testDeserialize() throws Exception {
-        // Arrange
-        var content = """
-                { "isbn": "1234567890", "title": "Title", "author": "Author", "price": 9.90 }""";
-        var book = new Book("1234567890", "Title", "Author", 9.90);
+	@Test
+	void testDeserialize() throws Exception {
+		// Arrange
+		var content = """
+			{ "isbn": "1234567890", "title": "Title", "author": "Author", "price": 9.90 }""";
+		var book = Book.of("1234567890", "Title", "Author", 9.90, null);
 
-        // Act
-        var result = json.parse(content);
+		// Act
+		var result = json.parse(content);
 
-        // Assert
-        assertThat(result).usingRecursiveComparison().isEqualTo(book);
-    }
+		// Assert
+		assertThat(result).usingRecursiveComparison().isEqualTo(book);
+	}
 }
